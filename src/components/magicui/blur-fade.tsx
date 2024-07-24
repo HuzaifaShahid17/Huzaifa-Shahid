bc5888204774e9b4df7e203a972d493c1a1ea0f5
Muhammad Azhan <126439037+adilsarfraz02@users.noreply.github.com>
2024-07-25 03:48:28 +0500

    update
    Co-Authored-By: Huzaifa Shahid <95350190+HuzaifaShahid17@users.noreply.github.com>

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AnimatePresence, motion, useInView, Variants } from "framer-motion";
-import { useRef, React } from "react";
+import React, { useRef, } from "react";
 
 interface BlurFadeProps {
   children: React.ReactNode;
